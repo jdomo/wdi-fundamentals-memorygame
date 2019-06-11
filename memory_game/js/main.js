68d46f4ac310e123194cf611d156d260b34abcf1
Joe Domovitch <jdomovitch@gmail.com>
2019-06-11 14:23:24 -0400

    added CSS, JS folders and files, linked to both in index

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+const cardOne = "queen";
+const cardTwo = "queen";
+const cardThree = "king";
+const cardFour = "king";
+
+console.log("User flipped a " + cardOne);
+console.log("User flipped a " + cardThree);

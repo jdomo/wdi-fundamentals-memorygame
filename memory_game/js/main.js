20120779b062ece99350684a1bbf00c45996790a
Joe Domovitch <jdomovitch@gmail.com>
2019-06-13 14:22:42 -0700

    added game logic v1 to main.js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,25 @@
 console.log("Up and running!");
 
-const cardOne = "queen";
-const cardTwo = "queen";
-const cardThree = "king";
-const cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped a " + cardOne);
-console.log("User flipped a " + cardThree);
+const cardsInPlay = [];
+
+const cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardOne);
+
+const cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+};
